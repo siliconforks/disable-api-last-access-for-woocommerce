72f350e376ba216d233ddef559717fc5302d9ed4
siliconforks <siliconforks@users.noreply.github.com>
2023-04-24 08:53:50 -0300

    Coding standards fixes

--- includes/query.php
+++ includes/query.php
@@ -18,38 +18,44 @@ if ( ! defined( 'ABSPATH' ) ) {
  *     } );
  */
 class Logger {
-	public static $is_enabled = FALSE;
+	public static $is_enabled = false;
 
 	public static $num_updates = 0;
 
 	public static function log_updates() {
 		if ( self::$num_updates > 0 ) {
-			if ( self::$num_updates === 1 ) {
+			if ( 1 === self::$num_updates ) {
 				$message = 'Disabled last access update';
-			}
-			else {
+			} else {
 				$message = sprintf( 'Disabled %d last access updates', self::$num_updates );
 			}
 
 			if ( isset( $_SERVER['REQUEST_URI'] ) ) {
-				$request_uri = $_SERVER['REQUEST_URI'];
-				$parameters = [
+				// phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
+				$request_uri = wp_unslash( $_SERVER['REQUEST_URI'] );
+
+				$parameters = array(
 					'oauth_consumer_key',
 					'oauth_nonce',
 					'oauth_signature',
 					'consumer_key',
 					'consumer_secret',
-				];
+				);
 				foreach ( $parameters as $parameter ) {
-					if ( isset( $_GET[$parameter] ) ) {
-						$sensitive = $_GET[$parameter];
+					// phpcs:disable WordPress.Security.NonceVerification.Recommended
+					if ( isset( $_GET[ $parameter ] ) ) {
+						// phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
+						$sensitive = wp_unslash( $_GET[ $parameter ] );
+
 						$replacement = '...' . substr( $sensitive, -7, 7 );
-						$request_uri = add_query_arg( $parameter, urlencode( $replacement ), $request_uri );
+						$request_uri = add_query_arg( $parameter, rawurlencode( $replacement ), $request_uri );
 					}
+					// phpcs:enable WordPress.Security.NonceVerification.Recommended
 				}
 				$message .= ' - ' . $request_uri;
 			}
 
+			// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
 			error_log( $message );
 		}
 	}
@@ -72,12 +78,12 @@ function query( $query ) {
 function add_query_filter() {
 	add_filter( 'query', __NAMESPACE__ . '\query' );
 	if ( Logger::$is_enabled ) {
-		add_action( 'shutdown', [ Logger::class, 'log_updates' ] );
+		add_action( 'shutdown', array( Logger::class, 'log_updates' ) );
 	}
 }
 
-// legacy API
+// Legacy WooCommerce API.
 add_action( 'woocommerce_api_loaded', __NAMESPACE__ . '\add_query_filter' );
 
-// WP REST API
+// WP REST API.
 add_action( 'rest_api_init', __NAMESPACE__ . '\add_query_filter' );
--- plugin.php
+++ plugin.php
@@ -1,10 +1,9 @@
 <?php
-
-/*
-Plugin Name: Disable API Last Access for WooCommerce
-Description: For every call to the WooCommerce REST API, WooCommerce updates a database field storing the last access time.  This plugin disables that feature entirely.
-Update URI: false
-*/
+/**
+ * Plugin Name: Disable API Last Access for WooCommerce
+ * Description: For every call to the WooCommerce REST API, WooCommerce updates a database field storing the last access time.  This plugin disables that feature entirely.
+ * Update URI: false
+ */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
